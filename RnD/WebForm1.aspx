c775d202efb58443207d06c84cea9cf5362a38f5
Mayank Pandey <Mayank@SDSBLR.COM>
2019-10-16 09:53:00 +0530

    Add project files.

---
+++ RnD/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RnD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <asp:Image ID="imgAdmin" ImageUrl="~/Images/admin.png" runat="server" />&nbsp;&nbsp;&nbsp;
+            <asp:Image ID="imgAircraft" ImageUrl="~/Images/aircraft.png" runat="server"/>&nbsp;&nbsp;&nbsp;
+            <textarea id="txtIP" runat="server"></textarea>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RnD/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RnD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var pattern = new Regex(@"(^172\.\d{1,3}\.\d{1,3}.\d{1,3}$)|(^192\.\d{1,3}\.\d{1,3}.\d{1,3}$)|(^10\.\d{1,3}\.\d{1,3}.\d{1,3}$)");
+                string ip;
+                ip = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+                if (ip == string.Empty || ip == null)
+                {
+                    ip = Request.ServerVariables["REMOTE_ADDR"];
+                }
+                if (!pattern.IsMatch(ip))
+                {
+                    imgAdmin.Visible = false;
+                }
+                txtIP.Value = ip;
+            }
+            catch(Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+    }
+}
